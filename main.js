4573e6122e7f6273da291a15f8c41a87bd214dbb
Villalba8 <107590858+Villalba8@users.noreply.github.com>
2022-07-29 10:34:52 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+// my current age
+var myAge = 26;
+//early years is equal to 2
+let earlyYears = 2 * 10.5;
+//my current age minus 2
+let laterYears = myAge -2;
+//later years multiplied by four 
+laterYears *= 4;
+// multiplies early years by later years to get my age in dog years
+var myAgeInDogYears = earlyYears + laterYears;
+// my namme in all lower case letters
+let myName = 'Ezekiel'.toLowerCase();
+// logs my actual age converted to dog years
+console.log(`my name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years. `);
 
